ca03ef14593562bd1c38df0ef5a0b53d9c0434bc
yash-04 <82692651+yash-04@users.noreply.github.com>
2022-10-14 12:09:36 +0530

    n=int(input()) a=n+1 b=n-1 count=0 if True:     for i in range(2,int(n**0.5)+1):         if n%i==0:             count+=1 if count==0:     print(0)
    else:
            
            while True:
                is_prime=True
                for i in range(2,int(a**0.5)+1):
                    if a%i==0:
                        is_prime=False
                        break
                if is_prime==True:
                        break
                else:
                        a+=1
    
            while True:
                is_prime2=True
                for i in range(2,int(b**0.5)+1):
                    if b%i==0:
                        is_prime2=False
                        break
                if is_prime2==True:
                        break
                else:
                        b-=1
            c=a-n
            d=n-b
            if c>d:
                print(d)
            elif d>c:
                print(c)
            else:
                print(d)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,42 @@
+n=int(input())
+a=n+1
+b=n-1
+count=0
+if True:
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            count+=1
+if count==0:
+    print(0)
+
+else:
+        
+        while True:
+            is_prime=True
+            for i in range(2,int(a**0.5)+1):
+                if a%i==0:
+                    is_prime=False
+                    break
+            if is_prime==True:
+                    break
+            else:
+                    a+=1
+
+        while True:
+            is_prime2=True
+            for i in range(2,int(b**0.5)+1):
+                if b%i==0:
+                    is_prime2=False
+                    break
+            if is_prime2==True:
+                    break
+            else:
+                    b-=1
+        c=a-n
+        d=n-b
+        if c>d:
+            print(d)
+        elif d>c:
+            print(c)
+        else:
+            print(d)
